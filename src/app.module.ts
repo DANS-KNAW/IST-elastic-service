10600c7de20f183f5197fc2a7bb98fe8fd87a5af
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-08-13 15:34:53 +0200

    Add ConfigModule, env validation and common config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import validationSchema from './config/validation-schema';
+import commonConfig from './config/common.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: false,
       envFilePath: ['.env.local', '.env.development', '.env.production'],
+      load: [commonConfig],
+      validationSchema,
+      validationOptions: {
+        allowUnknown: true, // Might want to set this to false in production.
+        abortEarly: true,
+      },
     }),
   ],
   controllers: [AppController],
