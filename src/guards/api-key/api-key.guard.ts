1be7d5d1f7c97695db3fc112d29907e6cccf6d09
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-08-21 01:38:55 +0200

    Removed placeholder guard

--- src/guards/api-key/api-key.guard.ts
+++ src/guards/api-key/api-key.guard.ts
@@ -13,34 +13,4 @@ export class ApiKeyGuard implements CanActivate {
     const apiKey = request.apiKey;
     return this.apiKeyService.validateApiKey(apiKey);
   }
-
-  // async (context: ExecutionContext): Promise<boolean> {
-  //   const request = context.switchToHttp().getRequest();
-  //   const apiKey = this.extractKeyFromHeader(request);
-  //   if (!apiKey) {
-  //     throw new UnauthorizedException();
-  //   }
-  //   const apiKeyEntityId = this.apiKeysService.extractIdFromApiKey(apiKey);
-  //   try {
-  //     const apiKeyEntity = await this.apiKeysRepository.findOne({
-  //       where: { uuid: apiKeyEntityId },
-  //       relations: { user: true },
-  //     });
-  //     await this.apiKeysService.validate(apiKey, apiKeyEntity.key);
-  //     request[REQUEST_USER_KEY] = {
-  //       sub: apiKeyEntity.user.id,
-  //       email: apiKeyEntity.user.email,
-  //       role: apiKeyEntity.user.role,
-  //       permissions: apiKeyEntity.user.permissions,
-  //     } as ActiveUserData;
-  //   } catch {
-  //     throw new UnauthorizedException();
-  //   }
-  //   return true;
-  // }
-
-  // private extractKeyFromHeader(request: Request): string | undefined {
-  //   const [type, key] = request.headers.authorization?.split(' ') ?? [];
-  //   return type === 'ApiKey' ? key : undefined;
-  // }
 }
