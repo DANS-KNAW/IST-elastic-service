961bd223dc0d9df283bbcec8aba740ec9fc98ad1
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-02-06 16:14:30 +0100

    Enabled micro service logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Get,
   HttpCode,
@@ -9,39 +8,39 @@ import {
   Req,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { MessagePattern, Payload } from '@nestjs/microservices';
-// import { MappingTypeMapping } from '@elastic/elasticsearch/lib/api/types';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MappingTypeMapping } from '@elastic/elasticsearch/lib/api/types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @MessagePattern({ cmd: 'create-index' })
-  // createIndex(
-  //   @Payload()
-  //   payload: {
-  //     alias: string;
-  //     mappings?: MappingTypeMapping;
-  //   },
-  // ): Promise<{ message: string; alias: string; indice: string }> {
-  //   return this.appService.createIndex(payload.alias, payload.mappings);
-  // }
+  @MessagePattern({ cmd: 'create-index' })
+  createIndex(
+    @Payload()
+    payload: {
+      alias: string;
+      mappings?: MappingTypeMapping;
+    },
+  ): Promise<{ message: string; alias: string; indice: string }> {
+    return this.appService.createIndex(payload.alias, payload.mappings);
+  }
 
-  // @MessagePattern({ cmd: 'index-document' })
-  // indexDocument(
-  //   @Payload()
-  //   payload: {
-  //     alias: string;
-  //     body: Record<string, any>;
-  //     customId?: string;
-  //   },
-  // ): Promise<{ message: string }> {
-  //   return this.appService.indexDocument(
-  //     payload.alias,
-  //     payload.body,
-  //     payload.customId,
-  //   );
-  // }
+  @MessagePattern({ cmd: 'index-document' })
+  indexDocument(
+    @Payload()
+    payload: {
+      alias: string;
+      body: Record<string, any>;
+      customId?: string;
+    },
+  ): Promise<{ message: string }> {
+    return this.appService.indexDocument(
+      payload.alias,
+      payload.body,
+      payload.customId,
+    );
+  }
 
   @Get()
   getHello(): string {
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import rmqConfig, { CONFIG_RMQ } from './config/rmq.config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
 
+  const configService = app.get(ConfigService);
+  const microConfig =
+    configService.get<ConfigType<typeof rmqConfig>>(CONFIG_RMQ);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [microConfig.connectionUri],
+    },
+  });
+
+  await app.startAllMicroservices();
+
   app.useBodyParser('json', { limit: '10mb' });
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
